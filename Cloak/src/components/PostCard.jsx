66a5da86915f4b9d6b37385e99e2aa35dfdc8c97
nitintwt <nitintwt27@gmail.com>
2024-01-30 05:06:10 +0530

    responsive postcard

--- Cloak/src/components/PostCard.jsx
+++ Cloak/src/components/PostCard.jsx
@@ -5,13 +5,22 @@ import { Link } from 'react-router-dom'
 function PostCard({$id, content}) {
 
   return (
-<Link to={`/post/${$id}`}>
-  <div className='flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2'>
-    <ScrollArea className="h-[250px] md:h-[200px] lg:h-[250px] xl:h-[300px] w-full md:w-[50%] lg:w-[33.3333%] xl:w-[25%] rounded-md border p-4">
-      {content}
-    </ScrollArea>
+    <div className='container mx-auto py-8'>
+    <div className='flex flex-wrap -mx-2'>
+      {posts.map((post) => (
+        <div key={post.$id} className='p-2 w-full md:w-1/2 lg:w-1/3 xl:w-1/4'>
+          <Link to={`/post/${post.$id}`} className="block w-full">
+            <div className='border rounded-md p-4'>
+              <ScrollArea className='h-[250px] md:h-[200px] lg:h-[250px] xl:h-[300px]'>
+                {post.content}
+              </ScrollArea>
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
   </div>
-</Link>
+  
 
   )
 }
