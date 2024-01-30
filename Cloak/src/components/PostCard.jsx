fcff436d8719ae6391696917c9b9214759cca397
nitintwt <nitintwt27@gmail.com>
2024-01-30 05:43:41 +0530

    none

--- Cloak/src/components/PostCard.jsx
+++ Cloak/src/components/PostCard.jsx
@@ -7,7 +7,7 @@ function PostCard({$id, content}) {
   return (
     <Link to={`/post/${$id}`} className="block w-full">
     <div className="flex p-4 border rounded-md mb-4">
-      <ScrollArea className="h-[250px] md:h-[250px] lg:h-[250px] xl:h-[300px] md:w-[340px]">
+      <ScrollArea className="h-[250px] md:h-[250px] lg:h-[250px] xl:h-[300px] md:w-[450px]">
         {content}
       </ScrollArea>
     </div>
